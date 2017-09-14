25d99d20d88ac04e8083d8aa960782b1a472f024
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-09-14 17:33:14 +0700

    add PrivateKeyReader, this class use for read private key from string

---
+++ src/main/kotlin/org/rizki/mufrizal/jwt/rsa/reader/PrivateKeyReader.kt
@@ -0,0 +1,40 @@
+package org.rizki.mufrizal.jwt.rsa.reader
+
+import java.security.spec.InvalidKeySpecException
+import java.security.NoSuchAlgorithmException
+import java.security.interfaces.RSAPrivateKey
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.KeyFactory
+import java.util.Base64
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since 14 September 2017
+ * @Time 1:19 PM
+ * @Project JWT-RSA
+ * @Package org.rizki.mufrizal.jwt.rsa.reader
+ * @File PrivateKeyReader
+ *
+ */
+class PrivateKeyReader {
+    companion object {
+        @JvmStatic
+        fun get(privateKey: String? = null): RSAPrivateKey? {
+            var privateKeyContent = privateKey
+            try {
+                privateKeyContent = privateKeyContent?.replace("\\n".toRegex(), "")?.replace("-----BEGIN PRIVATE KEY-----", "")?.replace("-----END PRIVATE KEY-----", "")
+                val kf = KeyFactory.getInstance("RSA")
+                val keySpecPKCS8 = PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKeyContent))
+                return kf.generatePrivate(keySpecPKCS8) as RSAPrivateKey
+            } catch (ex: NoSuchAlgorithmException) {
+                ex.printStackTrace()
+            } catch (ex: InvalidKeySpecException) {
+                ex.printStackTrace()
+            }
+
+            return null
+        }
+    }
+}
