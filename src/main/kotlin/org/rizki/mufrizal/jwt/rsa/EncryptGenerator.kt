485084535331411ab6b45aebc2ac2f4c8d7077fa
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-09-20 20:27:05 +0700

    add 2 function for encrypt with string or file public key

--- src/main/kotlin/org/rizki/mufrizal/jwt/rsa/EncryptGenerator.kt
+++ src/main/kotlin/org/rizki/mufrizal/jwt/rsa/EncryptGenerator.kt
@@ -7,7 +7,10 @@ import com.nimbusds.jose.JWEHeader
 import com.nimbusds.jose.JWEObject
 import com.nimbusds.jose.Payload
 import com.nimbusds.jose.crypto.RSAEncrypter
-import org.rizki.mufrizal.jwt.rsa.reader.PublicKeyReader
+import org.rizki.mufrizal.jwt.rsa.helpers.isEndSeparator
+import org.rizki.mufrizal.jwt.rsa.readers.PublicKeyReader
+import org.rizki.mufrizal.jwt.rsa.readers.PublicKeyReaderStorage
+import java.io.File
 import java.security.NoSuchAlgorithmException
 import java.security.spec.InvalidKeySpecException
 
@@ -30,8 +33,30 @@ class EncryptGenerator {
                 val header = JWEHeader(JWEAlgorithm.RSA_OAEP_256, EncryptionMethod.A256CBC_HS512)
                 val payload = Payload(plainText)
                 val jweObject = JWEObject(header, payload)
-                val encrypter = RSAEncrypter(PublicKeyReader.get(publicKey))
-                jweObject.encrypt(encrypter)
+                val publicKeyReader = PublicKeyReader.get(publicKey)
+                val encrypted = RSAEncrypter(publicKeyReader)
+                jweObject.encrypt(encrypted)
+                return jweObject.serialize()
+            } catch (ex: NoSuchAlgorithmException) {
+                ex.printStackTrace()
+            } catch (ex: InvalidKeySpecException) {
+                ex.printStackTrace()
+            } catch (ex: JOSEException) {
+                ex.printStackTrace()
+            }
+            return null
+        }
+
+        @JvmStatic
+        fun generateEncrypt(plainText: String? = null, path: String? = null, fileName: String? = null): String? {
+            try {
+                val header = JWEHeader(JWEAlgorithm.RSA_OAEP_256, EncryptionMethod.A256CBC_HS512)
+                val payload = Payload(plainText)
+                val jweObject = JWEObject(header, payload)
+                val pathSeparator = if (path?.isEndSeparator() == false) "$path${File.separator}$fileName" else "$path$fileName"
+                val publicKeyReader = PublicKeyReaderStorage.get(pathSeparator)
+                val encrypted = RSAEncrypter(publicKeyReader)
+                jweObject.encrypt(encrypted)
                 return jweObject.serialize()
             } catch (ex: NoSuchAlgorithmException) {
                 ex.printStackTrace()
