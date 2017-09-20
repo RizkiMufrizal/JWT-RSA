c68324d24e39903e410d1db680d6c4a9621ec871
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-09-20 20:26:49 +0700

    add 2 function for decrypt with string or file private key

--- src/main/kotlin/org/rizki/mufrizal/jwt/rsa/DecryptGenerator.kt
+++ src/main/kotlin/org/rizki/mufrizal/jwt/rsa/DecryptGenerator.kt
@@ -3,9 +3,12 @@ package org.rizki.mufrizal.jwt.rsa
 import com.nimbusds.jose.JOSEException
 import java.security.spec.InvalidKeySpecException
 import java.security.NoSuchAlgorithmException
-import org.rizki.mufrizal.jwt.rsa.reader.PrivateKeyReader
+import org.rizki.mufrizal.jwt.rsa.readers.PrivateKeyReader
 import com.nimbusds.jose.crypto.RSADecrypter
 import com.nimbusds.jose.JWEObject
+import org.rizki.mufrizal.jwt.rsa.helpers.isEndSeparator
+import org.rizki.mufrizal.jwt.rsa.readers.PrivateKeyReaderStorage
+import java.io.File
 import java.text.ParseException
 
 /**
@@ -24,10 +27,32 @@ class DecryptGenerator {
         @JvmStatic
         fun generateDecrypt(encryptText: String? = null, privateKey: String? = null): String? {
             try {
-                val jWEObject = JWEObject.parse(encryptText)
-                val decrypter = RSADecrypter(PrivateKeyReader.get(privateKey))
-                jWEObject.decrypt(decrypter)
-                return jWEObject.payload.toString()
+                val jweObject = JWEObject.parse(encryptText)
+                val privateKeyReader = PrivateKeyReader.get(privateKey)
+                val decrypted = RSADecrypter(privateKeyReader)
+                jweObject.decrypt(decrypted)
+                return jweObject.payload.toString()
+            } catch (ex: ParseException) {
+                ex.printStackTrace()
+            } catch (ex: NoSuchAlgorithmException) {
+                ex.printStackTrace()
+            } catch (ex: InvalidKeySpecException) {
+                ex.printStackTrace()
+            } catch (ex: JOSEException) {
+                ex.printStackTrace()
+            }
+            return null
+        }
+
+        @JvmStatic
+        fun generateDecrypt(encryptText: String? = null, path: String? = null, fileName: String? = null): String? {
+            try {
+                val jweObject = JWEObject.parse(encryptText)
+                val pathSeparator = if (path?.isEndSeparator() == false) "$path${File.separator}$fileName" else "$path$fileName"
+                val privateKeyReader = PrivateKeyReaderStorage.get(pathSeparator)
+                val decrypted = RSADecrypter(privateKeyReader)
+                jweObject.decrypt(decrypted)
+                return jweObject.payload.toString()
             } catch (ex: ParseException) {
                 ex.printStackTrace()
             } catch (ex: NoSuchAlgorithmException) {
